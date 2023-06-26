7d16b74e71aaf27562d7a172378a181ed5fca80d
Jcangrejo <cangre98@hotmail.com>
2023-06-26 09:26:19 -0500

    limpiar back

--- srvAppUcc/src/main/java/co/edu/ucc/app/repository/IAhorroRepository.java
+++
@@ -1,9 +0,0 @@
-package co.edu.ucc.app.repository;
-
-import co.edu.ucc.app.modeloCanonico.entities.AhorroDAO;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.math.BigDecimal;
-
-public interface IAhorroRepository extends JpaRepository<AhorroDAO, BigDecimal> {
-}
--- srvAppUcc/src/main/java/co/edu/ucc/app/repository/IGastoRepository.java
+++
@@ -1,9 +0,0 @@
-package co.edu.ucc.app.repository;
-
-import co.edu.ucc.app.modeloCanonico.entities.GastoDAO;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.math.BigDecimal;
-
-public interface IGastoRepository extends JpaRepository<GastoDAO, BigDecimal> {
-}
